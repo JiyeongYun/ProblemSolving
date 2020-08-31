ab5a880c55dd69ef8d7fb1ea81c5444b26ebdb7c
JiyeongYun <yoonjy1106@naver.com>
2020-09-01 01:19:58 +0900

    1330.java

---
+++ BOJ/1330.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+
+    if(num1 > num2){
+      System.out.println(">");
+    }else if(num1 < num2){
+      System.out.println("<");
+    }else{
+      System.out.println("==");
+    }
+  }
+}
+
