9323db0313c6acdf023034658180bc21057d4d0f
JiyeongYun <yoonjy1106@naver.com>
2020-09-06 01:24:20 +0900

    2742.java

---
+++ BOJ/2742.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for(int i = num; i > 0; i--){
+			System.out.println(i);
+		}
+		
+	}
+}
