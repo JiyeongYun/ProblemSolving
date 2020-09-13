535f8ca74accd53f82069998c0f836bd9259edaf
JiyeongYun <yoonjy1106@naver.com>
2020-09-14 01:31:37 +0900

    11021.java

---
+++ BOJ/11021.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int cnt = sc.nextInt();
+    int[] addArr = new int[cnt];
+
+    for(int i = 0; i < addArr.length; i++){
+      int n1 = sc.nextInt();
+      int n2 = sc.nextInt();
+      addArr[i] = n1+n2;
+    }
+
+    for(int i = 0; i < addArr.length; i++){
+      System.out.printf("Case #%d: %d\n", i+1, addArr[i]);
+    }
+
+  }
+}
+
