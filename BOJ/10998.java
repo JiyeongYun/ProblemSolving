4d2ee221642fc5e15b0c7346ec72946f9c53abc7
JiyeongYun <yoonjy1106@naver.com>
2020-09-18 01:35:21 +0900

    10998.java

---
+++ BOJ/10998.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        
+        if(0<num1&&num2<10){
+            int result = num1*num2;
+            System.out.println(result);
+        }
+    }
+}
