b19940a6e84abfc4f544a5858f876b318448443a
JiyeongYun <yoonjy1106@naver.com>
2021-01-19 19:54:01 +0900

    21.01.19 PS

---
+++ BOJ/BOJ11654.java
@@ -0,0 +1,10 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((int)sc.next().charAt(0));
+	}
+}
