1314ed227971e2f873611f47944960170b7797e5
JiyoungYoon <30459523+JiyeongYun@users.noreply.github.com>
2021-01-28 17:24:07 +0900

    Update BOJ2447.java

--- BOJ/BOJ2447.java
+++ BOJ/BOJ2447.java
@@ -9,29 +9,23 @@ public class BOJ2447 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		arr = new char[n][n];
-		
-		// Fill in the array with blank spaces
-		for(int i = 0; i < n; i++) {
+							
+		for(int i = 0; i < n; i++) {			// Fill in the array with blank spaces
 			Arrays.fill(arr[i],' ');
 		}
-		
-		// Call a recursive function: print(pointX, pointY, size)
-		print(0, 0, n);
-		
-		// Print the array one row at a time
-		for(int i = 0; i < n; i++) {
+		print(0, 0, n);					// Call a recursive function: print(pointX, pointY, size)
+		for(int i = 0; i < n; i++) {			// Print the array one row at a time
 			System.out.println(arr[i]);
 		}
 	}
 	
 	public static void print(int x, int y, int n) {
-		if(n == 1) { 						// Point at end of recursive call
+		if(n == 1) { 					// Point at end of recursive call
 			arr[x][y] = '*';
 			return;
 		}
-		
-		int div = n / 3; 					// Continue to divide the input n values by 3
-		for(int i = 0; i < 3; i++) { 		// Make a star 3x3 size.
+		int div = n / 3; 				// Continue to divide the input n values by 3
+		for(int i = 0; i < 3; i++) { 			// Make a star 3x3 size.
 			for(int j = 0; j < 3; j++) {
 				if(i == 1 && j == 1)		
 					continue;
