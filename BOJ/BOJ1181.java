ced96c828ba306f4cdad0d69174dd59fffad6628
JiyoungYoon <30459523+JiyeongYun@users.noreply.github.com>
2021-02-03 00:44:47 +0900

    Update BOJ1181.java

--- BOJ/BOJ1181.java
+++ BOJ/BOJ1181.java
@@ -31,42 +31,38 @@ public class BOJ1181 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        ArrayList<Word> words = new ArrayList<>();
+        	BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        	StringBuilder sb = new StringBuilder();
+        	ArrayList<Word> words = new ArrayList<>();
         
-        int t = Integer.parseInt(in.readLine());
-        for(int i = 0; i < t; i++) {
-        	String word = in.readLine();
-        	words.add(new Word(word.length(), word));		
-        }
-        
-        Collections.sort(words, new Comparator<Word>() {
-            public int compare(Word o1, Word o2) {
-                if(o1.l > o2.l) {
-                    return 1;
-                }else if(o1.l < o2.l) {
-                    return -1;
-                }else {
-                	return o1.w.compareTo(o2.w);
-                }
-            }
-        });
-        
-        String before = "";
-        for(int i = 0; i < words.size(); i++) {
-        	String now = words.get(i).w;
-        	if(!now.equals(before)) {
-        		sb.append(words.get(i).w).append("\n");
-        		before = now;
+        	int t = Integer.parseInt(in.readLine());
+        	for(int i = 0; i < t; i++) {
+        		String word = in.readLine();
+        		words.add(new Word(word.length(), word));		
         	}
-        	
-        }
         
-        out.write(sb.toString());
-        out.close();
-        in.close();
-
+        	Collections.sort(words, new Comparator<Word>() {
+        		public int compare(Word o1, Word o2) {
+                		if(o1.l > o2.l) {
+					return 1;
+                		}else if(o1.l < o2.l) {
+                    			return -1;
+                		}else {
+                			return o1.w.compareTo(o2.w);
+                		}
+            		}
+        	});
+        
+       	 	String before = "";
+        	for(int i = 0; i < words.size(); i++) {
+        		String now = words.get(i).w;
+        		if(!now.equals(before)) {
+        			sb.append(words.get(i).w).append("\n");
+        			before = now;
+        		}
+        	}
+        	out.write(sb.toString());
+        	out.close();
+        	in.close();
 	}
-
 }
