5e5521cb90f948f822d0ecffa11e7cf8a093e478
JiyeongYun <yoonjy1106@naver.com>
2021-02-23 18:18:09 +0900

    [Java] BOJ 10163

---
+++ BOJ/BOJ10163.java
@@ -0,0 +1,56 @@
+package boj;
+
+/**
+ * 2021-02-22
+ * BOJ 10163 - 색종이 
+ * Memory: 12108KB
+ * Execution Time: 84ms
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ10163 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int[][] map = new int[101][101];
+        int N = Integer.parseInt(in.readLine());
+        int[] counting = new int[N+1];
+        
+        for(int i = 1; i <= N; i++) {
+        	StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        	int x = Integer.parseInt(st.nextToken());
+        	int y = Integer.parseInt(st.nextToken());
+        	int w = Integer.parseInt(st.nextToken());
+        	int h = Integer.parseInt(st.nextToken());
+        	
+        	for(int j = 0; j < w; j++) {
+        		for(int k = 0; k < h; k++) {
+        			map[x+j][y+k] = i;
+        		}
+        	} 	
+        }
+        
+        for(int i = 0; i < 101; i++) {
+        	for(int j = 0; j < 101; j++) {
+        		counting[map[i][j]] += 1;
+        	}
+        }
+        
+        for(int i = 1; i <= N; i++) {
+        	sb.append(counting[i]).append("\n");
+        }
+        
+        
+        out.write(sb.toString());
+        out.close(); in.close();
+	}
+}
