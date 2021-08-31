8d78a31b67b0820dde9fa95adb83be9b84886864
jiyeongYun <yoonjy1106@naver.com>
2021-08-31 23:19:02 +0900

    [Java] Update BOJ 1654

--- BOJ/BOJ1654.java
+++ BOJ/BOJ1654.java
@@ -9,19 +9,18 @@ public class BOJ1654 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		
 		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
 		int K = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
 		int lines[] = new int[K];
 	
+		// 랜선 정보 입력받기
 		for(int i = 0; i < K; i++) {
 			lines[i] = Integer.parseInt(in.readLine());
 		}
 		Arrays.sort(lines);
 		
 		long left = 1, right = lines[K-1];
-		long answer = 1;
 		while(left <= right) {		
 			long mid = (left + right) / 2;
 			long sum = 0;
@@ -31,15 +30,9 @@ public class BOJ1654 {
 				sum += lines[i]/mid; 
 			}
 			
-			if(N <= sum) {
-				answer = Math.max(answer, sum);
-				left = mid + 1;
-			}
-				
-			else
-				right = mid - 1;
+			if(N <= sum) left = mid + 1;
+			else right = mid - 1;
 		}
-		
 		System.out.println(right);
 		in.close();
 
